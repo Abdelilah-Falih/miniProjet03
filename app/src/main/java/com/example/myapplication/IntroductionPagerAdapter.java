1739d2bc062e14311f399ccb08f0aaf73270f3a2
Abdelilah-Falih <121343981+Abdelilah-Falih@users.noreply.github.com>
2023-06-23 18:03:21 +0100

    task01 :  -> add pager  -> setup the item_pager_introduction and his custom adapter  -> show introduction text in the pager  -> start new Activity on the click at Skip

--- app/src/main/java/com/example/myapplication/IntroductionPagerAdapter.java
+++ app/src/main/java/com/example/myapplication/IntroductionPagerAdapter.java
@@ -11,40 +11,34 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class FoodPagerAdapter extends RecyclerView.Adapter<FoodPagerAdapter.ViewHolder> {
-    ArrayList<Food> foods ;
+public class IntroductionPagerAdapter extends RecyclerView.Adapter<IntroductionPagerAdapter.ViewHolder> {
+    String[] introduction_texts;
 
-    public FoodPagerAdapter(ArrayList<Food> foods) {
-        this.foods = foods;
+    public IntroductionPagerAdapter(String[] introduction_texts) {
+        this.introduction_texts = introduction_texts;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.pager_item_foods, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.pager_item_introduction, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Food food = foods.get(position);
-        holder.iv_food.setImageResource(food.getImageRes());
-        holder.tv_name.setText(food.getName());
-        holder.tv_description.setText(food.getDescription());
+        holder.tv_description.setText(introduction_texts[position]);
     }
 
     @Override
     public int getItemCount() {
-        return foods.size();
+        return introduction_texts.length;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        ImageView iv_food;
-        TextView tv_name, tv_description;
+        TextView tv_description;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            iv_food = itemView.findViewById(R.id.iv_food);
-            tv_name = itemView.findViewById(R.id.tv_name);
             tv_description = itemView.findViewById(R.id.tv_description);
         }
     }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,22 +2,18 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.myapplication.databinding.ActivityMainBinding;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
     View root;
 
-    ArrayList<Food> foods = new ArrayList<>();
-    int[] imagesRes = {R.drawable.tacos, R.drawable.denner, R.drawable.breakfast, R.drawable.lunch,};
-    String[] food_names = {"Tacos", "Dinner", "Breakfast", "Lunch"};
-    String[] food_desc;
+    String[] introduction_texts = {"Hi", "This app Let You Manage Your Quotes", "Enjoy .."};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,22 +23,15 @@ public class MainActivity extends AppCompatActivity {
         root = binding.getRoot();
         setContentView(root);
 
-        food_desc = getResources().getStringArray(R.array.food_desc);
 
 
-        for (int i = 0; i < imagesRes.length; i++) {
-            foods.add(new Food(imagesRes[i], food_names[i], food_desc[i]));
-        }
-
-        FoodPagerAdapter adapter = new FoodPagerAdapter(foods);
+        IntroductionPagerAdapter adapter = new IntroductionPagerAdapter(introduction_texts);
         binding.vPager.setAdapter(adapter);
-        binding.vPager.setClipToPadding(false);
-        binding.vPager.setClipChildren(false);
-        binding.vPager.setOffscreenPageLimit(2);
         binding.vPager.getChildAt(0).setOverScrollMode(View.OVER_SCROLL_NEVER);
 
-
-
+        binding.btnNext.setOnClickListener(v->{
+            startActivity(new Intent(this, ManageQuotes.class));
+        });
 
 
     }
