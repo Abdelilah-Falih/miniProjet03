886196ea83e6220220f3f36579c14295a0294118
Abdelilah-Falih <121343981+Abdelilah-Falih@users.noreply.github.com>
2023-06-24 00:42:00 +0100

    task 04 :  -> load image using glide

--- app/src/main/java/com/example/myapplication/ManageQuotes.java
+++ app/src/main/java/com/example/myapplication/ManageQuotes.java
@@ -3,13 +3,33 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.Random;
 
 public class ManageQuotes extends AppCompatActivity {
+    String[] images_links;
+    int x ;
+    ImageView iv_image_loaded;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_quotes);
+
+        iv_image_loaded = findViewById(R.id.iv_loadded_image);
+
+        images_links = getResources().getStringArray(R.array.images_links);
+        x = new Random().nextInt(5);
+
+        Glide.with(this)
+                .load(images_links[x])
+                .into(iv_image_loaded);
+
+
+
     }
 
 
